2fe63ae3e2d28cb5ef46b0f6ccaec51d6ccdeaa6
Yihui Xie <xie@yihui.name>
2023-04-04 17:49:59 -0500

    tweak the implementation of sidenotes

--- static/js/features.js
+++ static/js/features.js
@@ -39,19 +39,6 @@
     );
   };
 
-  // <a><b>c</b></a> -> <b><a>c</a></b>
-  var insideOut = function(el) {
-    var p = el.parentNode, x = el.innerHTML,
-      c = document.createElement('div');  // a tmp container
-    insertAfter(p, c);
-    c.appendChild(el);
-    el.innerHTML = '';
-    el.appendChild(p);
-    p.innerHTML = x;  // let the original parent have the content of its child
-    insertAfter(c, c.firstElementChild);
-    removeEl(c);
-  }
-
   var i, a, s;
 
   // process single articles
@@ -63,38 +50,33 @@
     insertAfter(el.parentNode, el);
   });
 
-  // move footnotes and citations to sidenotes
+  // move footnotes (ids start with fn) and citations (ids start with ref-) to sidenotes
   if (config.indexOf('-sidenotes') === -1) {
-    d.querySelectorAll('.footnotes > ol > li[id^="fn"], #refs > div[id^="ref-"]').forEach(function(fn) {
-      a = d.querySelectorAll('a[href="#' + fn.id + '"]');  // <a> that points to note id in body
-      if (a.length === 0) return;
-      a.forEach(function(el) { el.removeAttribute('href') });
-      a = a[0];
-      s = d.createElement('div');  // insert a side div next to n in body
+    d.querySelectorAll('.footnotes > ol > li[id^="fn"], #refs > div[id^="ref-"]').forEach(el => {
+      // find <a> that points to note id in body
+      const h = `a[href="#${el.id}"]`,
+        a = d.querySelector(`${h} > sup, sup > ${h}, .citation > ${h}`);
+      if (!a) return;
+      const a2 = a.parentNode;
+      (a.tagName === 'A' ? a : a2).removeAttribute('href');
+      const s = d.createElement('div');  // insert a side div next to n in body
       s.className = 'side side-right';
-      if (/^fn/.test(fn.id)) {
-        s.innerHTML = fn.innerHTML;
-        var n = a.innerText;   // footnote number
-        s.firstElementChild.innerHTML = '<span class="bg-number">' + n +
-          '</span> ' + s.firstElementChild.innerHTML;
-        removeEl(s.querySelector('a[href^="#fnref"]'));  // remove backreference
-        a.parentNode.tagName === 'SUP' && insideOut(a);
+      if (/^fn/.test(el.id)) {
+        s.innerHTML = el.innerHTML;
+        // add footnote number
+        s.firstElementChild.insertAdjacentHTML('afterbegin', `<span class="bg-number">${a.innerText}</span> `);
+        s.querySelector('a[href^="#fnref"]')?.remove();  // remove backreference
       } else {
-        s.innerHTML = fn.outerHTML;
-        a = a.parentNode;
+        s.innerHTML = el.outerHTML;
       }
       // insert note after the <sup> or <span> that contains a
-      insertAfter(a, s);
-      a.classList.add('note-ref');
-      removeEl(fn);
+      a2.after(s);
+      a2.classList.add('note-ref');
+      el.remove();
     });
     // remove the footnote/citation section if it's empty now
-    d.querySelectorAll('.footnotes, #refs').forEach(function(fn) {
-      var items = fn.children;
-      if (fn.id === 'refs') return items.length === 0 && removeEl(fn);
-      // there must be a <hr> and an <ol> left
-      if (items.length !== 2 || items[0].tagName !== 'HR' || items[1].tagName !== 'OL') return;
-      items[1].childElementCount === 0 && removeEl(fn);
+    d.querySelectorAll('.footnotes, #refs').forEach(el => {
+      /^\s*$/.test(el.innerText) && el.remove();
     });
   }
 
